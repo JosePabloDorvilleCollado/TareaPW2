94d9b8bc15eb79fe1cc8232d31501b8e160b15a3
JosePablo <JosePablo@jp>
2017-09-26 19:02:32 -0400

    Primer Commit con logica

---
+++ src/main/java/JP/Genesis.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JP;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author JosePablo
+ */
+public class Genesis {
+    public static void main(String[] args) {
+       SpringApplication.run(Genesis.class, args); 
+    }
+}
